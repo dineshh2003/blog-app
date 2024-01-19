6b01508e71266ee1695d9ffa6c3c626ae29b812a
Dinesh Jangid <spideyyyyy89@gmail.com>
2024-01-19 15:01:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Register from "./pages/register/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const user = false;
+  const user = true;
 
   return (
     <div className="App">
